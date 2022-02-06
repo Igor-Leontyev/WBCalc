e6461135f18cb4a306053f6dfeaf33007b019d48
Igor-Leontyev <79319381+Igor-Leontyev@users.noreply.github.com>
2022-02-06 16:30:52 +0300

    added DBHelper and CalcResult
    { Classes for DataBase working)

---
+++ app/src/main/java/com/crybz/wbcalc/CalcResult.kt
@@ -0,0 +1,14 @@
+package com.crybz.wbcalc
+
+class CalcResult {
+
+    var marginality:    Long = 0
+    var moneyTurnOver:  Long = 0
+    var oneThingProfit: Long = 0
+    var fullProfit:     Long = 0
+    var mp_taxes:       Long = 0 // market place percent
+    var logicsticCosts: Long = 0
+    var buyCosts:       Long = 0
+    var taxes:          Long = 0
+
+}
